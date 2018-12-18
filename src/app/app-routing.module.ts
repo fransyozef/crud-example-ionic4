6b434f7da578d0556e826a3b4a4d85e7b81a0470
Fransjo Leihitu <info@coreelements.nl>
2018-12-18 17:48:33 +0100

    setting default to items page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'items', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'items', loadChildren: './items/items.module#ItemsPageModule' },
   { path: 'item-edit', loadChildren: './items/item-edit/item-edit.module#ItemEditPageModule' },
