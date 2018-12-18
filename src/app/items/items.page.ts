ffdce78d934534f5e68ecb4990da31b413fd5eaf
Fransjo Leihitu <info@coreelements.nl>
2018-12-18 23:20:34 +0100

    item edit page

--- src/app/items/items.page.ts
+++ src/app/items/items.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject,throwError, of , BehaviorSubject} from 'rxjs';
+
+import { ItemModel } from './_models/item.model';
+import { ItemsService } from './_services/items.service';
 
 @Component({
   selector: 'app-items',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsPage implements OnInit {
 
-  constructor() { }
+  items$: BehaviorSubject<ItemModel[]>;
+
+  constructor(
+    private itemsService: ItemsService,
+  ) { }
 
   ngOnInit() {
+    this.items$  = this.itemsService.items$;
+  }
+
+  hasItems(items: ItemModel[]): boolean {
+    return items && items.length > 0 ? true : false;
   }
 
 }
